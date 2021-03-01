8a8fef948a3b6df1acb1623e8395817d8014de14
Adeleke Awwal <59998909+codemaestro02@users.noreply.github.com>
2021-03-01 20:31:22 +0100

    Add files via upload

---
+++ Forms/forms.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        You are highly welcome, <?php echo $_POST['username'];?> <br>
+        Your name is registered as <?php echo $_POST['name'];?> <br>
+        Your Date of Birth is registered as <?php echo $_POST['date'];?> <br>
+        Your email address is registered as <?php echo $_POST['email'];?> <br>
+        Your password is safe with us, <?php echo $_POST['username'];?>
+    </body>
+</html>
+
